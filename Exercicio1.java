5d7add0a5d3b4602d4503f7cdf56da6f78835b78
IsabelaM <belamaravilhosa516@gmail.com>
2026-02-20 20:12:52 +0000

    Exercicio 1 concluido

--- Exercicio1.java
+++ Exercicio1.java
@@ -7,15 +7,40 @@ public class Exercicio1 {
 	public static void main(String[] args) {
 		 Scanner input = new Scanner(System.in);   
 		 
-		 int x, y; // primeiro número e segundo número 
+		 int x;
+		 int y; // primeiro número e segundo número 
 		 int soma, subt, div, mult; // operações  
 		 
-		 System.out.print("Digite o primeiro número:" );   
+		 System.out.print("Digite o primeiro número: " );   
 		 x = input.nextInt();   
-		 System.out.print("Digite o segundo número:" );   
+		 System.out.print("Digite o segundo número: " );   
 		 y = input.nextInt();   
 		 
-		 //escreva seu código aqui
+		 while(true) {
+			Scanner entrada = new Scanner(System.in);
+	        System.out.println("Escolha a operação: 1 - Soma, 2 - Subtração, 3- Multiplicação, 4 - Divisão");
+
+			if(entrada.hasNextInt()) {
+				int numeros = entrada.nextInt();
+				soma = x + y;
+				System.out.println("A soma é: " + soma);
+			} else if(entrada.hasNextInt()) {
+				int numeros = entrada.nextInt();
+				subt = x - y;
+				System.out.println("A subtração é: " + subt);
+			} else if(entrada.hasNextInt()) {
+				int numeros = entrada.nextInt();
+				mult = x*y;
+				System.out.println("A multiplicação é: " + mult);
+			} else if(entrada.hasNextInt()) {
+				int numeros = entrada.nextInt();
+				div = x/y;
+				System.out.println("A divisão é: " + div);
+			} else {
+				System.out.println("Opção inválida.");
+			}
+			break;
+		 }
 
 		 input.close();
 	}
