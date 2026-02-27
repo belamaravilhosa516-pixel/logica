3d6f2840c184f5949c133add53e2132867b642b7
IsabelaM <belamaravilhosa516@gmail.com>
2026-02-27 17:30:21 +0000

    Exercício 22 concluído

--- Exercicio22.java
+++ Exercicio22.java
@@ -17,7 +17,28 @@ public class Exercicio22 {
 		System.out.print("Digite o terceiro valor: ");
 		num3 = input.nextInt();
 		
-		 //escreva seu código aqui
+		if (num1 < num2 && num1 < num3) {
+			System.out.print(num1 + " ");
+			if (num2 < num3) {
+				System.out.println(num2 + " " + num3);
+			} else {
+				System.out.println(num3 + " " + num2);
+			}
+		} else if (num2 < num1 && num2 < num3) {
+			System.out.print(num2 + " ");
+			if (num1 < num3) {
+				System.out.println(num1 + " " + num3);
+			} else {
+				System.out.println(num3 + " " + num1);
+			}
+		} else {
+			System.out.print(num3 + " ");
+			if (num1 < num2) {
+				System.out.println(num1 + " " + num2);
+			} else {
+				System.out.println(num2 + " " + num1);
+			}
+		}
 		
 		input.close();
 	}
