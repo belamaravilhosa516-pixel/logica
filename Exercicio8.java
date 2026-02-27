f10523affca3e857ae60df21572eefc36f847c3c
IsabelaM <belamaravilhosa516@gmail.com>
2026-02-27 17:01:37 +0000

    Exercício 8 concluído

--- Exercicio8.java
+++ Exercicio8.java
@@ -8,7 +8,19 @@ public class Exercicio8 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in); 
 
-		 //escreva seu código aqui
+		 System.out.print("Digite o número de maçãs compradas: ");
+		 int quantidade = input.nextInt();
+		 
+		 double preco;
+		 if (quantidade < 12) {
+			 preco = 0.30;
+		 } else {
+			 preco = 0.25;
+		 }
+		 
+		 double custoTotal = quantidade * preco;
+		 
+		 System.out.println("Custo total da compra: R$ " + custoTotal);
 
 		input.close();
 	}
