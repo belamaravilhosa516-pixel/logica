279b6bc6ba1639f377ce4847ad6215d19adef01d
IsabelaM <belamaravilhosa516@gmail.com>
2026-02-27 17:28:35 +0000

    Exercício 20 concluído

--- Exercicio20.java
+++ Exercicio20.java
@@ -7,7 +7,17 @@ import java.util.Scanner;
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in); 
 
-		//escreva seu código aqui
+		System.out.print("Digite o ano de nascimento: ");
+		int anoNascimento = input.nextInt();
+		
+		int anoAtual = 2024;
+		int idade = anoAtual - anoNascimento;
+		
+		if (idade >= 16) {
+			System.out.println("Você pode votar este ano.");
+		} else {
+			System.out.println("Você não pode votar este ano.");
+		}
 		
 		input.close();
 	}
