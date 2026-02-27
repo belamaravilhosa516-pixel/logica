fb343f6c621de813cc11a834fe82f2dd3abe73d7
IsabelaM <belamaravilhosa516@gmail.com>
2026-02-27 17:24:57 +0000

    Exercício 17 concluído

--- Exercicio17.java
+++ Exercicio17.java
@@ -18,7 +18,10 @@ public class Exercicio17 {
 		cubo = Math.pow(num, 3);
 		raiz = Math.sqrt(num);
 		
-		//escreva seu código aqui
+		System.out.println("Número:" + num);
+		System.out.println("Quadrado: " + quadrado);
+		System.out.println("Cubo: " + cubo);
+		System.out.println("Raiz quadrada: " + raiz);
 			
 		input.close();
 	}
