5d7ea4e70bd325fd9aa80329c8ad3abffacca663
IsabelaM <belamaravilhosa516@gmail.com>
2026-02-27 17:33:59 +0000

    Exercício 25 concluído

--- Exercicio25.java
+++ Exercicio25.java
@@ -8,7 +8,29 @@ public class Exercicio25 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-		 //escreva seu código aqui
+		 System.out.print("Digite o número de lados do polígono regular: ");
+		 int numLados = input.nextInt();
+		
+		 if (numLados < 3) {
+			 System.out.println("NÃO É UM POLÍGONO.");
+		 } else if (numLados > 5) {
+			 System.out.println("POLÍGONO NÃO IDENTIFICADO.");
+		 } else {
+			 System.out.print("Digite a medida do lado (em cm): ");
+			 double medidaLado = input.nextDouble();
+			 
+			 if (numLados == 3) {
+				 double area = (medidaLado * medidaLado * Math.sqrt(3)) / 4;
+				 System.out.println("TRIÂNGULO");
+				 System.out.println("Área: " + area + " cm²");
+			 } else if (numLados == 4) {
+				 double area = medidaLado * medidaLado;
+				 System.out.println("QUADRADO");
+				 System.out.println("Área: " + area + " cm²");
+			 } else if (numLados == 5) {
+				 System.out.println("PENTÁGONO");
+			 }
+		 }
 		
 		input.close();
 	}
