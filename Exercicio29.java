22e4d56a87fa8b70b9f7b6215f3cebe42444ef90
IsabelaM <belamaravilhosa516@gmail.com>
2026-02-27 17:40:10 +0000

    Exercício 29 concluído

--- Exercicio29.java
+++ Exercicio29.java
@@ -6,8 +6,14 @@ public class Exercicio29{
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in); 
 
-		//escreva seu código aqui
+		System.out.print("Digite um número: ");
+		int num = input.nextInt();
 		
+		if (num % 3 == 0) {
+			System.out.println("O número é múltiplo de 3.");
+		} else {
+			System.out.println("O número não é múltiplo de 3.");
+		}
 		input.close();
 	}
 }
