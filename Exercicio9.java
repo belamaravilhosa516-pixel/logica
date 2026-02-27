6e273a29885b8b9095366b3162167fc8fc92f724
IsabelaM <belamaravilhosa516@gmail.com>
2026-02-27 17:04:13 +0000

    Exercício 9 concluído

--- Exercicio9.java
+++ Exercicio9.java
@@ -8,8 +8,26 @@ public class Exercicio9 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in); 
 
-		 //escreva seu código aqui
+		 System.out.print("Digite a idade do nadador: ");
+		 int idade = input.nextInt();
 
+		 String categoria;
+		 
+		 if (idade < 5) {
+			 categoria = "nenhuma";
+		 } else if (idade >= 5 && idade <= 7) {
+			 categoria = "infantil";
+		 } else if (idade >= 8 && idade <= 10) {
+			 categoria = "juvenil";
+		 } else if (idade >= 11 && idade <= 15) {
+			 categoria = "adolescente";
+		 } else if (idade >= 16 && idade <= 30) {
+			 categoria = "adulto";
+		 } else {
+			 categoria = "sênior";
+		 }
+		 
+		 System.out.println("Categoria: " + categoria);
 		input.close();
 	}
 }
