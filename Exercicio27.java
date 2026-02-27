dfc02656a22e5e394457b561603a02fdd509ec70
IsabelaM <belamaravilhosa516@gmail.com>
2026-02-27 17:37:25 +0000

    Exercício 27 concluído

--- Exercicio27.java
+++ Exercicio27.java
@@ -9,7 +9,20 @@ public class Exercicio27 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-		 //escreva seu código aqui
+		 System.out.print("Digite o primeiro lado do triângulo: ");
+		 int lado1 = input.nextInt();
+		 System.out.print("Digite o segundo lado do triângulo: ");
+		 int lado2 = input.nextInt();
+		 System.out.print("Digite o terceiro lado do triângulo: ");
+		 int lado3 = input.nextInt();
+		
+		 if (lado1 == lado2 && lado2 == lado3) {
+			 System.out.println("Triângulo Equilátero");
+		 } else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+			 System.out.println("Triângulo Isósceles");
+		 } else {
+			 System.out.println("Triângulo Escaleno");
+		 }
 		
 		input.close();
 	}
