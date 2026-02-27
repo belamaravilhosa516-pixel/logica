316c837bcbca3321262cb91729a087e382aa5178
IsabelaM <belamaravilhosa516@gmail.com>
2026-02-27 17:05:21 +0000

    Exercício 10 concluído

--- Exercicio10.java
+++ Exercicio10.java
@@ -6,7 +6,14 @@ public class Exercicio10 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in); 
 
-		//escreva seu código aqui
+		System.out.print("Digite um valor inteiro: ");
+		int valor = input.nextInt();
+		
+		if (valor % 2 == 0) {
+			System.out.println("O valor é par.");
+		} else {
+			System.out.println("O valor é ímpar.");
+		}
 		
 		input.close();
 	}
