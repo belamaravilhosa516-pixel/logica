61cf0b4fbb08cd105259fc8e48ca431fca8a44d5
IsabelaM <belamaravilhosa516@gmail.com>
2026-02-27 17:09:23 +0000

    Exercício 12 concluído

--- Exercicio12.java
+++ Exercicio12.java
@@ -6,8 +6,40 @@ public class Exercicio12 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in); 
 
-		 //escreva seu código aqui
+		 System.out.print("Digite um número inteiro entre 1 e 12: ");
+		int mes = input.nextInt();
+
+		String nomeMes;
 		
+		 if (mes == 1) {
+			 nomeMes = "janeiro";
+		 } else if (mes == 2) {
+			 nomeMes = "fevereiro";
+		 } else if (mes == 3) {
+			 nomeMes = "março";
+		 } else if (mes == 4) {
+			 nomeMes = "abril";
+		 } else if (mes == 5) {
+			 nomeMes = "maio";
+		 } else if (mes == 6) {
+			 nomeMes = "junho";
+		 } else if (mes == 7) {
+			 nomeMes = "julho";
+		 } else if (mes == 8) {
+			 nomeMes = "agosto";
+		 } else if (mes == 9) {
+			 nomeMes = "setembro";
+		 } else if (mes == 10) {
+			 nomeMes = "outubro";
+		 } else if (mes == 11) {
+			 nomeMes = "novembro";
+		 } else if (mes == 12) {
+			 nomeMes = "dezembro";
+		 } else {
+			 nomeMes = "número inválido";
+		 }
+		 
+		 System.out.println("Mês correspondente: " + nomeMes);
 		input.close();
 	}
 }
