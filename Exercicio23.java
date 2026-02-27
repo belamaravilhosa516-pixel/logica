3d9731b0111a2e59193eb38a63ec435a7167dfe2
IsabelaM <belamaravilhosa516@gmail.com>
2026-02-27 17:31:25 +0000

    Exercício 23 concluído

--- Exercicio23.java
+++ Exercicio23.java
@@ -12,7 +12,25 @@ public class Exercicio23 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		//escreva seu código aqui
+		System.out.print("Digite o número de lados do polígono regular: ");
+		int numLados = input.nextInt();
+		
+		System.out.print("Digite a medida do lado (em cm): ");
+		double medidaLado = input.nextDouble();
+		
+		if (numLados == 3) {
+			double area = (medidaLado * medidaLado * Math.sqrt(3)) / 4;
+			System.out.println("TRIÂNGULO");
+			System.out.println("Área: " + area + " cm²");
+		} else if (numLados == 4) {
+			double area = medidaLado * medidaLado;
+			System.out.println("QUADRADO");
+			System.out.println("Área: " + area + " cm²");
+		} else if (numLados == 5) {
+			System.out.println("PENTÁGONO");
+		} else {
+			System.out.println("Número de lados inválido.");
+		}
 		
 		input.close();
 	}
